a3ddd8ed7e10bd2f2bbb08a31c15c29a5d2dbe15
Alan Ordorica <alaniordorica@gmail.com>
2021-09-22 14:43:48 -0700

    used require for backend

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
-import express from "express";
-import path, { dirname } from 'path';
+const path = require("path");
+const express = require("express");
+const app = express();
 
-const app = express()
+const port = process.env.PORT || 3000;
+const publicDir = path.join(__dirname, "../public");
 
-const port = process.env.PORT || 3000
-const publicDir = path.join(__dirname, '../public')
+app.use(express.static(publicDir));
 
-app.use(express.static(publicDir))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicDir, 'index.html'))
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(publicDir, "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Application is running on port: ${port}`);
-})
-
+});
